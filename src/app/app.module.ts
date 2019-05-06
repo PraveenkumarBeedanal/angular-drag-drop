b68b4e45604075b990d701eed25de951e5808dc3
PraveenkumarBeedanal <st147769@stud.uni-stuttgart.de>
2019-05-06 09:13:49 +0200

    initialize the ng2-file-upload library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import {UploadComponent} from './ng2-file-upload/upload.component';
 
 /**------primeng module -----**/
 import { DragDropModule } from 'primeng/dragdrop';
@@ -10,7 +11,7 @@ import { TableModule } from 'primeng/table';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, DragDropModule, TableModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, UploadComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
